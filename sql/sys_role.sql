91aea928fc5a6712faed5938aa29090b441db74c
wangjy <1358762936@qq.com>
2024-09-02 15:57:46 +0800

    update

---
+++ sql/sys_role.sql
@@ -0,0 +1,2 @@
+INSERT INTO `sys_role`(`id`, `name`, `code`, `remark`, `status`, `deleted`, `version`, `create_by`, `update_by`, `gmt_create`, `gmt_modified`) VALUES (145997280873615361, '管理员', 'admin', '预设管理员角色', 1, 0, 0, 1, 1, '2022-11-23 16:46:44', '2024-05-09 10:02:57');
+INSERT INTO `sys_role`(`id`, `name`, `code`, `remark`, `status`, `deleted`, `version`, `create_by`, `update_by`, `gmt_create`, `gmt_modified`) VALUES (145997282974961664, '普通角色', 'user', '预设用户角色1', 1, 0, 0, 1, 145997278759686144, '2024-05-14 16:48:54', '2024-05-15 14:55:01');
