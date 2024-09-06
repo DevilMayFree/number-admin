104cbd05dfa686312dfc961a840f3693912bf099
wangjy <1358762936@qq.com>
2024-09-06 09:14:56 +0800

    update

--- admin/src/main/java/com/freeying/admin/number/domain/query/NumManagerExportQuery.java
+++ admin/src/main/java/com/freeying/admin/number/domain/query/NumManagerExportQuery.java
@@ -3,6 +3,7 @@ package com.freeying.admin.number.domain.query;
 import com.freeying.common.core.entity.DTO;
 import com.freeying.common.core.entity.LocalDateTimeRange;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
@@ -23,8 +24,12 @@ public class NumManagerExportQuery extends DTO {
     @Schema(description = "客户过期时间天数")
     private String expiryDateNum;
 
+    private Long longExpiryDateNum;
+
     @Schema(description = "卡片过期时间天数")
     private String cardExpiryDateNum;
+
+    private Long longCardExpiryDateNum;
 
     public String getKeywords() {
         return keywords;
@@ -48,6 +53,13 @@ public class NumManagerExportQuery extends DTO {
 
     public void setExpiryDateNum(String expiryDateNum) {
         this.expiryDateNum = expiryDateNum;
+        if (StringUtils.isNotBlank(expiryDateNum)) {
+            try {
+                this.longExpiryDateNum = Long.parseLong(expiryDateNum);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("请输入有效数值查询");
+            }
+        }
     }
 
     public String getCardExpiryDateNum() {
@@ -56,6 +68,21 @@ public class NumManagerExportQuery extends DTO {
 
     public void setCardExpiryDateNum(String cardExpiryDateNum) {
         this.cardExpiryDateNum = cardExpiryDateNum;
+        if (StringUtils.isNotBlank(cardExpiryDateNum)) {
+            try {
+                this.longCardExpiryDateNum = Long.parseLong(cardExpiryDateNum);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("请输入有效数值查询");
+            }
+        }
+    }
+
+    public Long getLongExpiryDateNum() {
+        return longExpiryDateNum;
+    }
+
+    public Long getLongCardExpiryDateNum() {
+        return longCardExpiryDateNum;
     }
 
     @Override
